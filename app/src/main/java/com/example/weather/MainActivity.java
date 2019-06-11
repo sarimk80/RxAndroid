8f74352311fb090d8e7a4a2cff70d14b33df5f49
sarim <sarimk80>
2019-06-11 13:16:26 +0500

    singles

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -7,8 +7,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.weather.model.Book;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
@@ -28,14 +32,66 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                Single<Book> bookSingle = getSingleBook();
+
+                bookSingle.observeOn(Schedulers.io())
+                        .subscribeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new SingleObserver<Book>() {
+                            @Override
+                            public void onSubscribe(Disposable d) {
+                                disposable = d;
+                            }
+
+                            @Override
+                            public void onSuccess(Book book) {
+                                Log.d("Main", book.getBookname() + " " + book.getId());
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+                        });
+
+                /*
+
+                Single<String> stringSingle = getSingleString();
+
+                stringSingle.observeOn(Schedulers.io()).subscribeOn(AndroidSchedulers.mainThread()).subscribe(new SingleObserver<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable = d;
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.d("Main", s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+
                 Observable<String> observable = getObservable();
                 Observer<String> observer = getObserver();
                 observable
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeOn(Schedulers.io())
                         .subscribe(observer);
+                        */
             }
         });
+    }
+
+    private Single<Book> getSingleBook() {
+        return Single.just(new Book("1", "The Richest man in Babylon"));
+    }
+
+    private Single<String> getSingleString() {
+        return Single.just("sarim");
     }
 
     private Observer<String> getObserver() {
